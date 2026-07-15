ca60509c0fa7b42b38bf72653a3b3138dcfca53e
Alvin Alexander <alvinalexraj@gmail.com>
2026-07-15 15:06:36 +0530

    Project Completed

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -8,6 +8,8 @@ int main()
 {
         Calculator c;
         std::cout<<c.add(4,5)<< " ";
+        std::cout<<c.sub(45,10)<< " ";
+        std::cout<<c.mul(45,10)<< " ";
         std::cout<<c.div(45,10);
         return 0;
 }
