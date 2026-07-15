90b227a1f704e88b55a1a6e2420740efa9aa7196
Ameer <ameermubarak@gmail.com>
2026-07-15 14:04:00 +0530

    Addition Feature Added

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,13 @@
+//
+// Created by hp on 15/07/2026.
+//
+
+#include "Calculator.h"
+#include<iostream>
+int main()
+{
+        Calculator c;
+        std::cout<<c.add(4,5);
+
+        return 0;
+}
---
+++ Calculator/Calculator.h
@@ -0,0 +1,19 @@
+//
+// Created by hp on 15/07/2026.
+//
+
+#ifndef DEVOPS_CALCULATOR_H
+#define DEVOPS_CALCULATOR_H
+
+
+class Calculator
+{
+public:
+    int add(int a,int b)
+    {
+        return a + b;
+    }
+};
+
+
+#endif //DEVOPS_CALCULATOR_H
